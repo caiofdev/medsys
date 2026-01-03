edebf50d946710aa15b2ac969872721dce418dd7
lucasferoli <lucasferoliveira.goval@gmail.com>
2026-01-02 22:41:03 -0300

    refactor(routes): Beautify the routes code

--- routes/web.php
+++ routes/web.php
@@ -14,84 +14,81 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-
-Route::middleware('user.type:admin')
+    Route::middleware('user.type:admin')
         ->prefix('admin')
         ->name('admin.')
         ->group(function () {
-        
             Route::get('dashboard', [DashboardController::class, 'adminDashboard'])->name('dashboard');
 
-            Route::controller(AdminController::class)->prefix('admins')->name('admins.')->group(function () {
-                Route::get('/', 'index')->name('index'); 
-                Route::post('/', 'store')->name('store');
-                Route::get('/{admin}', 'show')->name('show');
-                Route::put('/{admin}', 'update')->name('update');
-                Route::delete('/{admin}', 'destroy')->name('destroy');
-            });
+            Route::controller(AdminController::class)
+                ->prefix('admins')
+                ->name('admins.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::post('/', 'store')->name('store');
+                    Route::get('/{admin}', 'show')->name('show');
+                    Route::put('/{admin}', 'update')->name('update');
+                    Route::delete('/{admin}', 'destroy')->name('destroy');
+                });
 
-            Route::controller(DoctorController::class)->prefix('doctors')->name('doctors.')->group(function () {
-                Route::get('/', 'index')->name('index');      
-                Route::post('/', 'store')->name('store');
-                Route::get('/{doctor}', 'show')->name('show');
-                Route::put('/{doctor}', 'update')->name('update');
-                Route::delete('/{doctor}', 'destroy')->name('destroy');
-            });
+            Route::controller(DoctorController::class)
+                ->prefix('doctors')
+                ->name('doctors.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::post('/', 'store')->name('store');
+                    Route::get('/{doctor}', 'show')->name('show');
+                    Route::put('/{doctor}', 'update')->name('update');
+                    Route::delete('/{doctor}', 'destroy')->name('destroy');
+                });
 
-            Route::controller(ReceptionistController::class)->prefix('receptionists')->name('receptionists.')->group(function () {
-                Route::get('/', 'index')->name('index');      
-                Route::post('/', 'store')->name('store');
-                Route::get('/{receptionist}', 'show')->name('show');
-                Route::put('/{receptionist}', 'update')->name('update');
-                Route::delete('/{receptionist}', 'destroy')->name('destroy');
-            });
+            Route::controller(ReceptionistController::class)
+                ->prefix('receptionists')
+                ->name('receptionists.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::post('/', 'store')->name('store');
+                    Route::get('/{receptionist}', 'show')->name('show');
+                    Route::put('/{receptionist}', 'update')->name('update');
+                    Route::delete('/{receptionist}', 'destroy')->name('destroy');
+                });
 
-            Route::controller(DashboardController::class)->prefix('reports')->name('reports.')->group(function () {
-                Route::get('last-consultations', 'getLastFiveCompletedConsultationsApi')->name('last-consultations');
-                Route::get('monthly-revenue', 'getMonthlyRevenueApi')->name('monthly-revenue');
-            });
+            Route::controller(DashboardController::class)
+                ->prefix('reports')
+                ->name('reports.')
+                ->group(function () {
+                    Route::get('last-consultations', 'getLastFiveCompletedConsultationsApi')->name('last-consultations');
+                    Route::get('monthly-revenue', 'getMonthlyRevenueApi')->name('monthly-revenue');
+                });
         });
-        
-Route::middleware('user.type:receptionist')
-    ->prefix('receptionist')
-    ->name('receptionist.')
-    ->group(function () {
-        Route::controller(PatientController::class)->prefix('patients')->name('patient.')->group(function () {
-        Route::get('{patient}', 'show')->name('show');
-        Route::post('/', 'store')->name('store');
-        Route::put('{patient}', 'update')->name('update');
-        Route::delete('{patient}', 'destroy')->name('destroy');
-        Route::get('/', 'index')->name('table');
+
+    Route::middleware('user.type:receptionist')
+        ->prefix('receptionist')
+        ->name('receptionist.')
+        ->group(function () {
+            Route::controller(PatientController::class)
+                ->prefix('patients')
+                ->name('patient.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('table');
+                    Route::get('{patient}', 'show')->name('show');
+                    Route::post('/', 'store')->name('store');
+                    Route::put('{patient}', 'update')->name('update');
+                    Route::delete('{patient}', 'destroy')->name('destroy');
+                });
+
+            Route::controller(AppointmentController::class)
+                ->prefix('appointments')
+                ->name('appointment.')
+                ->group(function () {
+                    Route::post('/', 'store')->name('store');
+                    Route::get('patients', 'getPatients')->name('patients');
+                    Route::get('doctors', 'getDoctors')->name('doctors');
+                });
+
+            Route::get('consultations-list', [DashboardController::class, 'consultationsList'])->name('consultations-list');
+            Route::get('dashboard', [DashboardController::class, 'receptionistDashboard'])->name('dashboard');
         });
-
-        Route::controller(AppointmentController::class)->prefix('appointments')->name('appointment.')->group(function () {
-        Route::post('/', 'store')->name('store');
-        Route::get('patients', 'getPatients')->name('patients');
-        Route::get('doctors', 'getDoctors')->name('doctors');
-        });
-        
-        Route::get('consultations-list', [DashboardController::class, 'consultationsList'])->name('consultations-list');
-        Route::get('dashboard', [DashboardController::class, 'receptionistDashboard'])->name('dashboard');
-    });
-
-    Route::middleware('user.type:doctor')
-    ->prefix('doctor')
-    ->name('doctor.')
-    ->group(function () {
-
-        Route::get('start-consultation', [DoctorController::class, 'startConsultation'])->name('start-consultation');
-        Route::post('finish-consultation', [DoctorController::class, 'finishConsultation'])->name('finish-consultation');
-        Route::get('medical-record', [DoctorController::class, 'medicalRecords'])->name('medical-record');
-        Route::get('medical-record/{patient}', [DoctorController::class, 'showMedicalRecord'])->name('medical-record.show');
-        Route::get('dashboard', [DashboardController::class, 'doctorDashboard'])->name('dashboard');
-
-    });
-
-
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    
-    
-
 
     Route::get('/csrf-token', function () {
         return response()->json(['token' => csrf_token()]);
