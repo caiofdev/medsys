6686cae847fe659d30b776f72c9509bd1519161e
Caio Reis <caiofreis2005@gmail.com>
2026-01-03 12:23:37 -0300

    feat: implement domain models for Admin, Appointment, Consultation, Doctor, Patient, Receptionist, and User with relationships and fillable attributes

--- app/Domain/Models/Admin.php
+++ app/Domain/Models/Admin.php
@@ -1,26 +1,21 @@
 <?php
 
-namespace App\Models;
+namespace App\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Doctor extends Model
+class Admin extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'crm',
+        'is_master',
         'user_id',
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function appointments()
-    {
-        return $this->hasMany(Appointment::class);
     }
 }
--- app/Domain/Models/Doctor.php
+++ app/Domain/Models/Doctor.php
@@ -1,21 +1,26 @@
 <?php
 
-namespace App\Models;
+namespace App\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Admin extends Model
+class Doctor extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'is_master',
+        'crm',
         'user_id',
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
     }
 }
