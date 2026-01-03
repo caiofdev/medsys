bc462f26bda1170a4e0fe3e3ab455288e3f55246
lucasferoli <lucasferoliveira.goval@gmail.com>
2026-01-02 22:18:52 -0300

    feat(routes): Add prefix, name and middleware to the admin routes

--- routes/web.php
+++ routes/web.php
@@ -14,33 +14,52 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
+Route::middleware('user.type:admin')
+        ->prefix('admin')
+        ->name('admin.')
+        ->group(function () {
+            
+            Route::get('dashboard', [DashboardController::class, 'adminDashboard'])->name('dashboard');
+
+            Route::controller(AdminController::class)->prefix('admins')->name('admins.')->group(function () {
+                Route::get('/', 'index')->name('index'); 
+                Route::post('/', 'store')->name('store');
+                Route::get('/{admin}', 'show')->name('show');
+                Route::put('/{admin}', 'update')->name('update');
+                Route::delete('/{admin}', 'destroy')->name('destroy');
+            });
+
+            Route::controller(DoctorController::class)->prefix('doctors')->name('doctors.')->group(function () {
+                Route::get('/', 'index')->name('index');      
+                Route::post('/', 'store')->name('store');
+                Route::get('/{doctor}', 'show')->name('show');
+                Route::put('/{doctor}', 'update')->name('update');
+                Route::delete('/{doctor}', 'destroy')->name('destroy');
+            });
+
+            Route::controller(ReceptionistController::class)->prefix('receptionists')->name('receptionists.')->group(function () {
+                Route::get('/', 'index')->name('index');      
+                Route::post('/', 'store')->name('store');
+                Route::get('/{receptionist}', 'show')->name('show');
+                Route::put('/{receptionist}', 'update')->name('update');
+                Route::delete('/{receptionist}', 'destroy')->name('destroy');
+            });
+
+            Route::controller(DashboardController::class)->prefix('reports')->name('reports.')->group(function () {
+                Route::get('last-consultations', 'getLastFiveCompletedConsultationsApi')->name('last-consultations');
+                Route::get('monthly-revenue', 'getMonthlyRevenueApi')->name('monthly-revenue');
+            });
+        });
+        
+    Route::get('receptionists/{receptionist}', [ReceptionistController::class, 'show'])->name('receptionist.show')->middleware('user.type:admin');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('admin/dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.dashboard')->middleware('user.type:admin');
     Route::get('doctor/dashboard', [DashboardController::class, 'doctorDashboard'])->name('doctor.dashboard')->middleware('user.type:doctor');
     Route::get('receptionist/dashboard', [DashboardController::class, 'receptionistDashboard'])->name('receptionist.dashboard')->middleware('user.type:receptionist');
 
-    Route::get('admin/admins', [AdminController::class, 'index'])->name('admin.table')->middleware('user.type:admin');
-    Route::get('admin/doctors', [DoctorController::class, 'index'])->name('doctor.table')->middleware('user.type:admin');
-    Route::get('admin/receptionists', [ReceptionistController::class, 'index'])->name('receptionist.table')->middleware('user.type:admin');
     Route::get('receptionist/patients', [PatientController::class, 'index'])->name('patient.table')->middleware('user.type:receptionist');
     
-    Route::get('admin/admins/{admin}', [AdminController::class, 'show'])->name('admin.show')->middleware('user.type:admin');
-    Route::post('admin/admins', [AdminController::class, 'store'])->name('admin.store')->middleware('user.type:admin');
-    Route::put('admin/admins/{admin}', [AdminController::class, 'update'])->name('admin.update')->middleware('user.type:admin');
-    Route::delete('admin/admins/{admin}', [AdminController::class, 'destroy'])->name('admin.destroy')->middleware('user.type:admin');
     
-    Route::get('admin/reports/last-consultations', [DashboardController::class, 'getLastFiveCompletedConsultationsApi'])->name('admin.reports.last-consultations')->middleware('user.type:admin');
-    Route::get('admin/reports/monthly-revenue', [DashboardController::class, 'getMonthlyRevenueApi'])->name('admin.reports.monthly-revenue')->middleware('user.type:admin');
-
-    Route::get('admin/doctors/{doctor}', [DoctorController::class, 'show'])->name('doctor.show')->middleware('user.type:admin');
-    Route::post('admin/doctors', [DoctorController::class, 'store'])->name('doctor.store')->middleware('user.type:admin');
-    Route::put('admin/doctors/{doctor}', [DoctorController::class, 'update'])->name('doctor.update')->middleware('user.type:admin');
-    Route::delete('admin/doctors/{doctor}', [DoctorController::class, 'destroy'])->name('doctor.destroy')->middleware('user.type:admin');
-
-    Route::get('admin/receptionists/{receptionist}', [ReceptionistController::class, 'show'])->name('receptionist.show')->middleware('user.type:admin');
-    Route::post('admin/receptionists', [ReceptionistController::class, 'store'])->name('receptionist.store')->middleware('user.type:admin');
-    Route::put('admin/receptionists/{receptionist}', [ReceptionistController::class, 'update'])->name('receptionist.update')->middleware('user.type:admin');
-    Route::delete('admin/receptionists/{receptionist}', [ReceptionistController::class, 'destroy'])->name('receptionist.destroy')->middleware('user.type:admin');
 
     Route::get('receptionist/patients/{patient}', [PatientController::class, 'show'])->name('patient.show')->middleware('user.type:receptionist');
     Route::post('receptionist/patients', [PatientController::class, 'store'])->name('patient.store')->middleware('user.type:receptionist');
