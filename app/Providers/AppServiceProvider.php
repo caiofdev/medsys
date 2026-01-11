c429c7417685366458c6370f337aabde8b2845d2
lucasferoli <lucasferoliveira.goval@gmail.com>
2026-01-11 19:03:20 -0300

    build: map custom model paths in AppServiceProvider to fix seeding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 use App\Domain\Models\Doctor;
 use App\Domain\Models\Receptionist;
 use App\Domain\Models\Appointment;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,5 +35,19 @@ class AppServiceProvider extends ServiceProvider
         Route::bind('receptionist', function ($value) {
             return Receptionist::with('user')->findOrFail($value);
         });
+
+        Factory::guessFactoryNamesUsing(function (string $modelName) {
+        $modelName = class_basename($modelName);
+        
+        return 'Database\\Factories\\' . $modelName . 'Factory';
+    });
+
+    Factory::guessModelNamesUsing(function (Factory $factory) {
+        $namespacedFactory = get_class($factory);
+        
+        $modelName = Str::replaceLast('Factory', '', class_basename($namespacedFactory));
+
+        return 'App\\Domain\\Models\\' . $modelName;
+    });
     }
 }
