e1da5d318215e9ea0cfdda2618b1fa4a8bccb734
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-01-15 15:51:53 -0300

    refactor: move files into dashboard folder

--- resources/js/components/dashboard/dashboard-card.tsx
+++ resources/js/components/dashboard/dashboard-card.tsx
@@ -13,7 +13,7 @@ export default function DashboardCard( { icon, title, color, route }: DashboardC
     return (
         <div 
             onClick={() => route && router.visit(route)}
-            className=" flex flex-col overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border h-full justify-center items-center gap-3 cursor-pointer hover:shadow-2xl hover:scale-102" style={{ backgroundColor: '#F7F2EB' }}
+            className=" flex flex-col overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border h-full justify-center items-center gap-3 cursor-pointer hover:shadow-2xl hover:scale-102 bg-digital-blue-50" 
         >
             <div className='flex flex-col justify-center items-center'>
                 <FontAwesomeIcon icon={icon} style={{ color: `#${color}` }} className="text-6xl" /> 
