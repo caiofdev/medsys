b22c3c5300a3cb15f47cd9584ebcaf0166d01e78
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-01-15 15:55:49 -0300

    refactor: reorganize sidebar imports and remove unused components

--- resources/js/components/app-shell.tsx
+++ resources/js/components/app-shell.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarProvider } from '@/components/sidebar/sidebar';
 import { SharedData } from '@/types';
 import { usePage } from '@inertiajs/react';
 
